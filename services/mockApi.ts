2e92d3800d532d5b13006fe913716d0c0b8626ae
kintwadi <joinemabi@gmail.com>
2026-02-11 15:10:47 +0100

    rest endpoint

--- services/mockApi.ts
+++ services/mockApi.ts
@@ -2,7 +2,8 @@
 import { MOCK_USERS } from '../data/mockData';
 import { Listing, User, Message, AvailabilityStatus, BorrowHistoryItem, Review, UserStatus, VerificationStatus } from '../types';
 
-export const API_BASE = (window as any).__API_BASE__ || 'http://localhost:8081';
+declare const __API_BASE__: string;
+export const API_BASE = __API_BASE__;
 const STORAGE_USER_ID = 'nearshare_current_user_id';
 const TOKEN_KEY = 'nearshare_token';
 
